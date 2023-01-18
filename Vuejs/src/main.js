56197d3d0eb184051bfc1e30ad20de64ec69f936
KucingBobok <afiksinggih@gmail.com>
2023-01-18 12:28:26 +0700

    back and front update

--- Vuejs/src/main.js
+++ Vuejs/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css"
 import router from './router'
 
 import './assets/main.css'
@@ -9,3 +10,6 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
+
